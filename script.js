30d12345e2ff52069f5c5987916baa8a09072c8a
cmash93 <cmash93@gmail.com>
2022-08-11 22:45:11 -0500

    added results

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ var results = {
 function playGame() {
     var userSelection = window.prompt("Please enter R[🪨], P[📃], or S[✂️].");
     window.alert("User chose " + userSelection);
+
+    var compSelection = Math.floor(Math.random() * options.length);
+    var compMove = options[compSelection]
+    window.alert(compMove);
+
+    if 
+    (userSelection === compMove) {
+        window.alert("It's a tie!")
+    }
+    
+    else if
+    ((userSelection === "R" && compMove === "S") || (userSelection === "S" && compMove === "P") || (userSelection === "P" && compMove === "R")) {
+        window.alert("You win!")
+    }
+
+    else
+    (window.alert("You lose!"))
 }
 
 
